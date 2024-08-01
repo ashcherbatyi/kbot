bd83d6478d2b0c3e55a1ea8b144190306e8191df
ashcherbatyi <alexey.shcherbatyi@gmail.com>
2024-08-01 17:57:43 +0000

    Fix Jenkins Pipeline

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -5,10 +5,37 @@ pipeline {
         choice(name: 'ARCH', choices: ['amd64', 'arm64'], description: 'Pick architecture', defaultValue: 'amd64')
     }
     stages {
-        stage('Example') {
+        stage("clone") {
             steps {
-                echo "Build for platform ${params.OS}"
-                echo "Build for arch: ${params.ARCH}"
+                echo 'Clone Repository'
+                git branch: 'main', url: 'https://github.com/ashcherbatyi/kbot.git'
+            }
+        }
+        stage("test") {
+            steps {
+                echo 'Tests Started'
+                sh 'make test'
+            }
+        }
+        stage("build") {
+            steps {
+                echo 'BUILD STARTED'
+                sh 'make build'
+            }
+        }
+        stage("image") {
+            steps {
+                echo 'Image Execution Started'
+                sh 'make image'
+            }
+        }
+        stage("push") {
+            steps {
+                script {
+                docker.withRegistry('', 'dockerhub') {
+                    sh 'make push'
+                    }               
+                }  
             }
         }
     }
